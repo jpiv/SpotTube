2c0f085ed8f7427f1dbb5ae89a7c80ff1182f0e1
jpiv <jjpizzo4@gmail.com>
2016-03-09 16:32:55 -0500

    Adds sequalize and persistant user table

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const server = require('express')();
+const sequelize = require('sequelize');
+const config = require('./config/config.json').development;
+
+const db = new sequelize(config.database, config.username, config.password, {
+	dialect: config.dialect,
+	port: config.port
+});
+const port = 3000;
+
+const User = db.define('User', {
+	username: {
+		type: sequelize.STRING,
+		allowNull: false
+	},
+	password: {
+		type: sequelize.STRING,
+		allowNull: false
+	}
+});
+
+db.authenticate().then(() => {
+	console.log('connected..');
+}).catch(err => {
+	console.log('Error:', err);
+});
+
+
+
+server.get('/', (req, res) => {
+	res.status(200).end();
+});
+
+db.sync({ force: true }).then(() => {
+	console.log('SYNC complete');
+	User.create({username: 'abc', password: '123'}).then(() => {
+		console.log('user created');
+	}).catch((err) => {
+		console.log('err', err)
+	});
+});
+
+
+server.listen(port);
